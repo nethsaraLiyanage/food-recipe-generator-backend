32386d03bbc608668e984aea7c24b09d17b79418
ashenvimanga <ashen@solistech.com>
2024-04-22 16:21:24 +0530

    fix : refactor code

--- src/Api/src/routes/auth.route.js
+++ src/Api/src/routes/auth.route.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const { login } = require("../api/auth.api");
 
-
 router.post("/", login);
 
 module.exports = router;
--- src/Api/src/routes/user.route.js
+++ src/Api/src/routes/user.route.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { saveUser } = require("../api/user.api");
 
-
 router.post("/", saveUser);
 
 module.exports = router;
